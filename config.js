fc8e48ee0d8d674a6146c0b77e1e995de1f7f908
Penguin-xanhlam <85724679+Penguin-xanhlam@users.noreply.github.com>
2021-06-11 20:21:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Hello anh!',
+    introTitle: 'Hello cậu!',
     introDesc: `Trên thế giới có hơn 7 tỉ nụ cười
-    mà nụ cười anh vẫn là đẹp nhất nên là làm ơn
-    hãy để em mãi giữ nụ cười của anh trên môi`,
+    mà nụ cười cậu vẫn là đẹp nhất nên là làm ơn
+    hãy để tớ mãi giữ nụ cười của cậu trên môi`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu em nhấttt <33',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu cậu nhấttt <33',
     btnNo: 'Ờ không ? :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Vì em đã xinh gái còn cuteeeeee <33333333',
-    mess: 'em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: ' IB cho em điiiii nhaaaa <333',
+    reply: 'Vì tớ đã đẹp trai còn cuteeeeee <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: ' IB cho tớ điiiii nhaaaa <333',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //https://www.facebook.com/cobexinhxan1812
+    messLink: 'http://fb.com' //https://www.facebook.com/thach3619/
 }
